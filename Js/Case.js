21a0a4fa6debf688afaa0c58818a242c21c97488
6Thienthien6 <thien502760@gmail.com>
2022-06-01 07:49:00 +0700

    010622

--- Js/Case.js
+++ Js/Case.js
@@ -5,11 +5,39 @@ class Product {
         this.productName = productName;
         this.quantity = quantity;
         this.price = price;
-
-
     }
 
 }
+
+var products = [new Product(1, "img/MSI-Delta-15-H2.jpeg", "MSI Delta15", 1, 35000000, ),
+    new Product(2, "img/asusF571GT.jpg", "AsusF571GT", 1, 23000000),
+    new Product(3, "img/msiGF63.png", "MSI GF63", 1, 20000000),
+    new Product(4, "img/msiGL75.jpg", "MSI GL73", 1, 25000000),
+    new Product(5, "img/msiGL75.jpg", "MSI GL65", 1, 23000000),
+    new Product(6, "img/msiGL75.jpg", "MSI GL71", 1, 24000000),
+    new Product(7, "img/msiGL75.jpg", "MSI GL78", 1, 29000000),
+    new Product(8, "img/msiGL75.jpg", "MSI GL62", 1, 22000000),
+    new Product(9, "img/mouselogitech_g102.jpg", "Logitech G102", 1, 180000),
+];
+// var products = []
+
+// function init() {
+//     if (localStorage.getItem("product_data") == null) {
+//         products = [new Product(1, "img/MSI-Delta-15-H2.jpeg", "MSI Delta15", 1, 35000000, ),
+//             new Product(2, "img/asusF571GT.jpg", "AsusF571GT", 1, 23000000),
+//             new Product(3, "img/msiGF63.png", "MSI GF63", 1, 20000000),
+//             new Product(4, "img/msiGL75.jpg", "MSI GL73", 1, 25000000),
+//             new Product(5, "img/msiGL75.jpg", "MSI GL65", 1, 23000000),
+//             new Product(6, "img/msiGL75.jpg", "MSI GL71", 1, 24000000),
+//             new Product(7, "img/msiGL75.jpg", "MSI GL78", 1, 29000000),
+//             new Product(8, "img/msiGL75.jpg", "MSI GL62", 1, 22000000),
+//             new Product(9, "img/mouselogitech_g102.jpg", "Logitech G102", 1, 180000),
+//         ]
+//         localStorage.setItem("product_data", JSON.stringify(products))
+//     } else {
+//         products = JSON.parse(localStorage.getItem("product_data"));
+//     }
+// }
 class Helper {
     static formatCurrency(number) {
         return number.toLocaleString("vi-VN", {
@@ -18,42 +46,40 @@ class Helper {
         });
     }
 }
-var products = [new Product(1, "img/MSI-Delta-15-H2.jpeg", "MSI Delta15", 1, 35000000, ),
-    new Product(2, "img/asusF571GT.jpg", "AsusF571GT", 1, 23000000),
-    new Product(3, "img/msiGF63.png", "MSI GF63", 1, 20000000),
-    new Product(4, "img/msiGL75.jpg", "MSI GL75", 1, 25000000),
-];
 
 
-
-function renderProduct(products) {
+function renderProduct(data) {
     let tbProduct = document.querySelector('.table>tbody');
-    let htmls = products.map(function(product) {
+    let html = data.map(function(product) {
         return `<tr id="tr_${product.productId}">
-        <td>${product.productId}</td>
-        <td> <img src=${product.avatar}></td>
+        <td >${product.productId}</td>
+        <td class ="text-right"><img src="${product.avatar}"></td>
         <td>${product.productName}</td>
-        <td>${product.quantity}</td>
-        <td>${Helper.formatCurrency(product.price)}</td>
-        <td><button class="btn btn-warning" onclick="change(${product.productId})">edit</button>
-            <button class="btn btn-danger" onclick="remove(${product.productId})">remove</button></td>
+        <td class ="text-right">${product.quantity}</td>
+        <td class ="text-right">${Helper.formatCurrency(product.price)}</td>
+        <td id ="action_${product.productId}" class='text-right'>
+        <button class="btn btn-warning" onclick="change(${product.productId})">edit</button>
+        <button class="btn btn-warning d-none" id="cancel-btn" onclick="cancel(${product.productId})">cancel</button>
+            <button class="btn btn-danger" onclick="remove(${product.productId})">remove</button>
+            </td>
 
     </tr>`;
 
     })
-    tbProduct.innerHTML = htmls.join("");
+    tbProduct.innerHTML = html.join("");
 }
 
 function Add() {
 
     let avatar = document.querySelector("#idavatar").value;
-    let productname = document.querySelector("#productname").value;
+    let productName = document.querySelector("#productname").value;
     let quantity = Number(document.querySelector("#quantity").value);
     let price = Number(document.querySelector("#price").value);
     let productId = getLastId() + 1;
-    let product = new Product(productId, avatar, productname, quantity, price);
+    let product = new Product(productId, avatar, productName, quantity, price);
 
     products.push(product);
+    //localStorage.setItem("product_data", JSON.stringify(products))
     renderProduct(products);
     resetForm();
 }
@@ -69,10 +95,15 @@ function getLastId() {
 }
 
 function resetForm() {
-    document.querySelector("#id_avatar").value = "";
+    document.querySelector("#idavatar").value = "";
     document.querySelector("#productname").value = "";
     document.querySelector("#quantity").value = "";
     document.querySelector("#price").value = "";
+    renderProduct(products);
+}
+
+function validation(field) {
+    return field != null && field.trim() != '';
 }
 
 function remove(productId) {
@@ -97,17 +128,54 @@ function change(pdtId) {
     let tr = document.getElementById(`tr_${pdtId}`);
     let product = getProductById(pdtId);
 
-    tr.children[1].innerHTML = `<input type='text' value = '${product.avatar}'/>`
-    tr.children[2].innerHTML = `<input type='text' value = '${product.productName}'/>`
-    tr.children[3].innerHTML = `<input type='text' value = '${product.quantity}'/>`
-    tr.children[4].innerHTML = `<input type='text' value = '${product.price}'/>`
+    tr.children[1].innerHTML = `<input type="text"  value = '${product.avatar}'/>`
+    tr.children[2].innerHTML = `<input  type="text"  value = '${product.productName}'/>`
+    tr.children[3].innerHTML = `<input  type="text" value = '${product.quantity}'/>`
+    tr.children[4].innerHTML = `<input type="text" value = '${product.price}'/>`
+    let action = document.getElementById(`action_${pdtId}`);
+    action.children[0].classList.add('d-none');
+    action.children[1].classList.remove('d-none');
+    action.children[2].classList.remove('d-none');
 
-};
 
-function updateProduct(pdtId) {
-    return
 }
 
+function cancel(pdtId) {
+    let tr = document.getElementById(`tr_${pdtId}`);
+    let product = getProductById(pdtId);
+
+    tr.children[1].innerHTML = `<img src="${product.avatar}">`;
+    tr.children[2].innerHTML = product.productName;
+    tr.children[3].innerHTML = product.quantity;
+    tr.children[4].innerHTML = Helper.formatCurrency(product.price);
+    let action = document.getElementById(`action_${pdtId}`);
+    action.children[0].classList.remove('d-none');
+    action.children[1].classList.add('d-none');
+    action.children[2].classList.add('d-none');
+}
+
+
+
+
+// function update(pdtId) {
+//     let tr = document.getElementById(`tr_${pdtId}`);
+//     let product = getProductById(pdtId);
+//     let newAvatar = tr.children[1].children[0].value;
+//     let newProductName = tr.children[1].children[0].value;
+//     let newQuantity = Number(tr.children[2].children[0].value);
+//     let newPrice = Number(tr.children[3].children[0].value);
+//     product.avatar = newAvatar;
+//     product.productName = newProductName;
+//     product.quantity = newQuantity;
+//     product.price = newPrice;
+// }
+
+// function cancel() {
+//     resetForm();
+//     // document.querySelector("#create-btn").classList.remove("d-none");
+//     document.querySelector("#update-btn").classList.add("d-none");
+//     document.querySelector("#cancel-btn").classList.add("d-none");
+// }
 
 
 
@@ -128,17 +196,20 @@ function sort(direct) {
 
 
 function search(searchInput) {
-    let result = products.filter(function(Product) {
-        return (Product.productName.toLowerCase().indexOf(searchInput.value.toLowerCase()) != -1) ||
-            Product.price == Number(searchInput.value)
+    let result = products.filter(function(product) {
+        return (
+            product.productName.toLowerCase().indexOf(searchInput.value.toLowerCase()) !=
+            -1 ||
+            product.price == Number(searchInput.value)
+        );
     });
     renderProduct(result);
 }
 
+
 function ready() {
     // init();
     renderProduct(products);
-
 
 }
 ready();
---
+++ Readme.md
@@ -0,0 +1,11 @@
+Các kiến thức áp dụng
+1. HTML/CSS
+2. DOM/BOM
+3. JavaScript (Array Methods, LocalStorage)
+4. OOP/ Object
+5. Function
+Theme sử dụng
+1. https://www.pinterest.com/pin/361625045089284286/visual-search/?imageSignature=a70ef9f7d85119a57c89d87cd4f3a843
+Kết quả đạt được
+1. Link mã nguồn:https://github.com/2hnono30/xe_repository
+2. Link sản phẩm:
